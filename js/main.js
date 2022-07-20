3f75edfaa8d36ef23c8bad1b2706ca2d50509ea3
sweetvenom1010 <57639832+sweetvenom1010@users.noreply.github.com>
2022-07-20 11:51:32 +0530

    JS files

---
+++ js/main.js
@@ -0,0 +1,18 @@
+//Swiper slider
+///This is kind of useless right now because all items of navigation bar have been turned off
+var swiper = new Swiper(".bg-slider-thumbs", {
+    loop: true,
+    spaceBetween: 0,
+    slidesPerView: 0,
+});
+var swiper2 = new Swiper(".bg-slider", {
+    loop: true,
+    spaceBetween: 0,
+    thumbs: {
+        swiper: swiper,
+    },
+});
+
+//Navigation bar effects on scroll
+
+//Responsive navigation menu toggle
